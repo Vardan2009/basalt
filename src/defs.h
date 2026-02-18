a596349774dbf26335ffc227d3c0df3fea397867
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-02-18 23:42:42 +0400

    Version 0.1.0

--- src/defs.h
+++ src/defs.h
@@ -3,8 +3,8 @@
 
 namespace defs {
 
-constexpr std::string_view basaltVersion = "0.0.0";
+constexpr std::string_view basaltVersion = "0.1.0";
 constexpr std::string_view basaltLink = "https://github.com/Vardan2009/basalt";
-constexpr std::string_view basaltLicenseNote = "No license yet";
+constexpr std::string_view basaltLicenseNote = "Licensed under the GNU GPL-3.0";
 
 }  // namespace defs
